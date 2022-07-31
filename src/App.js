6c28fcfcf6d72c97a29b5683ed6c527221016a88
Martinpouh <martinchege2015@gmail.com>
2022-07-31 22:44:08 +0300

    Create a component folder and create a Header.js file and write code to add navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import MealList from './MealList';
+import Header from "./Header";
+import Home from "./Home";
+import NewMealForm from './NewMealForm';
+import MealDetail from './MealDetail';
+import NotFound from './NotFound';
+import './App.css'
+import './Navbar.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Navbar, Container, Button } from "react-bootstrap";
+import {Link} from "react-router-dom";
+import "../styles/NewMealForm.css"
+
+
+
+function Header() {
+  return (
+    <Navbar className="header">
+        <Container>
+            <Link to={"/"}>
+                <Navbar.Brand>Belafonte-Food-Hub
+</Navbar.Brand>
+            </Link>
+
+            <Link to={"/meals"}>
+                <Navbar.Text>Meals</Navbar.Text>
+            </Link>
+
+            <Link to={"/create"}>
+                <Button className="btn-custom" > Add Meal </Button>
+            </Link>
+        </Container>
+    </Navbar>
+  )
+}
+
+export default Header
