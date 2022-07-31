540d0d6e5bf9e42aeb9793ab65cae597b4d6a7d9
Martinpouh <martinchege2015@gmail.com>
2022-07-31 22:58:43 +0300

    Create a card to hold the meal image and text describing each meal

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -4,3 +4,27 @@ import { Link } from "react-router-dom";
 
 function MealCard({meal, handleCardClick}) {
     const {strMeal, strArea, description, meallink} = meal;
+    return (
+        <Col>
+        <Link to={`/meals/${meal.idMeal}`} >
+            <Card
+                border="light" 
+                style={{ width: "20rem" }} 
+                className="mealCard" 
+                onClick={() => handleCardClick(meal)}
+            >
+                <Card.Img variant="top" src={meallink} />
+                <Card.Body>
+                    <Card.Title className="mealCardTitle">{strMeal}</Card.Title>
+                    <h4 class="card-title">{strArea}</h4>
+                    
+                    <Card.Text className="italicize xs line-clamp3">
+                        {description}
+                    </Card.Text>
+                    
+                </Card.Body>
+            </Card>
+        </Link>
+        </Col>
+    );
+}
