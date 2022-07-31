60d39f66da20561e8dc2ba3cc119a544c0ce5cf3
Martinpouh <martinchege2015@gmail.com>
2022-07-31 22:53:17 +0300

    Create Home component and create a function home with prop meals

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "../styles/Home.css"
+import { Link, useHistory } from "react-router-dom";
+
+function Home({meals}) {
+    const history = useHistory();
+
+    function handleClick(e) {
+        switch (e.target.innerHTML) {
+            case "Create":
+                history.push("/create");
+                break;
+            case "Explore":
+                history.push("/meals");
+                break;
+            case "Hungry?":
+                const randomIndex = Math.floor(Math.random() * meals.length);
+                const randomMeal = meals[randomIndex];
+                console.log(randomMeal);
+                history.push(`/meals/${randomMeal.idMeal}`);
+                break;
+            default:
+        }
+    }
