f6788a4d496a50368f6228e22842698d053d182b
Martinpouh <martinchege2015@gmail.com>
2022-07-31 23:09:40 +0300

    Create a menuitem component

---
+++ src/components/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'meals',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+]
