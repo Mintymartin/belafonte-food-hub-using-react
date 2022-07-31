250f7d4c87c2e0d036b99fdd09e8bf2191d4ee97
Martinpouh <martinchege2015@gmail.com>
2022-07-31 23:14:24 +0300

    Create a component Not found to swing in when there is an error

---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../styles/NotFound.css"
+
+function NotFound() {
+  return (
+    <div className="notFound">
+        <h1>Apologies!</h1>
+        <h2>Sorry, Come back later.</h2>
+    </div>
+  )
+}
+
+export default NotFound
