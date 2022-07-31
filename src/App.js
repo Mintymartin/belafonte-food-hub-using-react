59df3c1c81ab51612430804a5bec97a17e2505d6
Martinpouh <martinchege2015@gmail.com>
2022-07-31 22:46:19 +0300

    Add the fetch API and async function

--- src/App.js
+++ src/App.js
@@ -9,3 +9,31 @@ import NotFound from './NotFound';
 import './App.css'
 import './Navbar.css'
 
+const mealsAPI = "http://localhost:8004/meals/";
+
+
+function App() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8004/meals/")
+     .then(res => res.json())
+     .then(data => setMeals(data))
+  }, []);
+
+  async function addMeal(meal) {
+    const res = await fetch(mealsAPI, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(meal),
+    });
+    const data = await res.json();
+    setMeals([...meals, data]);
+    return data;
+  }
+
+  function handleCardClick(meal) {
+    // console.log("Meal Name: ",Meal.name);
+  }
