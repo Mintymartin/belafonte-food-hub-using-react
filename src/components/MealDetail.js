8f8ef30b23d362aa73e78fd5bd4212d4516e5a93
Martinpouh <martinchege2015@gmail.com>
2022-07-31 23:05:00 +0300

    Modify the MealDetail component

--- src/components/MealDetail.js
+++ src/components/MealDetail.js
@@ -5,7 +5,7 @@ import { PlusCircleFill } from 'react-bootstrap-icons';
 import NotFound from "./NotFound"
 import "../styles/MealDetail.css";
 
-const mealAPI = "/";
+const mealAPI = "http://localhost:8004/meals/";
 
 function MealDetail() {
     const [meal, setMeal] = useState({});
@@ -16,3 +16,55 @@ function MealDetail() {
         .then(res => res.json())
         .then(data => setMeal(data))
     }, []);
+
+    function renderDetails() {
+        if (Object.keys(meal).length !== 0) {
+            return (
+                <div className="mealDetailContainer">
+                    <div className="detail-summary">
+                        <div className="detailImage">
+                            <Image src={meal.meallink}/>
+                        </div>
+                        <h1 className="mealDetailName">{meal.strMeal}</h1>
+                    </div>
+                    <div className="meal-info">
+                        <h3 className="bold">Ingredients</h3>
+                        <div className="ingredientsList">
+                            {meal.description.map(ing => {
+                                return (
+                                    <div key={ing.strMeal} className="listItem" >
+                                        <PlusCircleFill/>
+                                        {` ${!ing.strSource? "" : ing.strSource} 
+                                        ${!ing.strMeal ? "" : ing.strMeal}`}
+                                    </div>
+                                )}
+                            )}
+                        </div>
+
+                        <h3 className="bold">Instructions</h3>
+                        <div className="ingredientsList">
+                            {meal.description.map(step => {
+                                return (
+                                    <div key={step} className="listItem" >
+                                        <PlusCircleFill/>
+                                        {` ${!step ? "" : step}`}
+                                    </div>
+                                )}
+                            )}
+                        </div>
+                    </div>
+                </div>
+            )
+        } else {
+            return <NotFound />
+        }
+    }
+    
+    return (
+        renderDetails()
+    )
+}
+
+export default MealDetail
+
+
